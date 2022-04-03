f46415c3049d21839fe2c8a3995bc90c62ff1458
Gl33ch3r <37208041+Gl33ch3r@users.noreply.github.com>
2022-04-03 21:21:40 +0800

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mount -oremount,rw /
+
+cd /tzwww/cgi-bin
+rm -R imei.sh
+wget https://raw.githubusercontent.com/Gl33ch3r/vn007/main/imei.sh
+chmod +x imei.sh
+cd ..
+rm -R changeImei.html
+wget https://raw.githubusercontent.com/Gl33ch3r/vn007/main/changeImei.html
