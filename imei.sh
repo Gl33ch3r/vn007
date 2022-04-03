684854f33bb8864a282954372e1b4cace9fa2f45
Gl33ch3r <37208041+Gl33ch3r@users.noreply.github.com>
2022-04-03 21:13:30 +0800

    Update imei.sh

--- imei.sh
+++ imei.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo "Content-type: text/html"
-echo ""
-echo "Hello World!"
+printf "Content-Type: text/html\r\n\r\n"
+#/usr/bin/env
+read line
+imei=`echo "$line" | awk '{split($0,array,"&")} END{print array[1]}' | awk '{split($0,array,"=")} END{print array[2]}'`
+comm=$(atcmd 'AT+SPIMEI=0,"'$imei'" -D')
+echo $comm
