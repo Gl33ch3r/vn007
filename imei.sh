7886edd8899145a2f9c36a71878490be0965395b
Gl33ch3r <37208041+Gl33ch3r@users.noreply.github.com>
2022-04-03 21:41:07 +0800

    Update imei.sh

--- imei.sh
+++ imei.sh
@@ -5,3 +5,4 @@ read line
 imei=`echo "$line" | awk '{split($0,array,"&")} END{print array[1]}' | awk '{split($0,array,"=")} END{print array[2]}'`
 comm=$(atcmd 'AT+SPIMEI=0,"'$imei'" -D')
 echo $comm
+reboot
